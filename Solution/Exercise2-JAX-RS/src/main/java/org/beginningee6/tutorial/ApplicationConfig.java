f1bbf0f787eb9d333cf96c5f44823d63af449bfd
Alexis Hassler <alexis@sewatech.org>
2012-04-12 22:20:21 +0200

    Gros ménage : il ne reste que 1 module pour la solution

--- Solution/Exercise2-JAX-RS/src/main/java/org/beginningee6/tutorial/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package org.beginningee6.tutorial;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class ApplicationConfig extends Application {
-}
